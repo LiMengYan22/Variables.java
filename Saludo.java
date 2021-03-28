8368a046ad821e417f1354ce1fec185b7c62b072
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-28 13:15:20 +0200

    Create Saludo.java

---
+++ Saludo.java
@@ -0,0 +1,9 @@
+public class Saludo {
+    public static void main(String args[]){
+        String saludo;
+        saludo = "Hola Mundo!!";
+        System.out.println(saludo);
+        saludo = ("Estoy utilizando variables");
+        System.out.println( saludo );
+    }
+}
